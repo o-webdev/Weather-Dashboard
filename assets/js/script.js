8d26d778ce502fc73b9d0a45bea4b0cb0bb1f00d
Omar-ahm <irycevier@gmail.com>
2023-01-30 11:09:58 +0000

    form submit function added and prompt for API key

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,8 +4,9 @@ $(document).ready(function() {
     var todayURL = 'https://api.openweathermap.org/data/2.5/weather?q=';
     var forcastURL = 'https://api.openweathermap.org/data/2.5/forecast?q=';
     
-    // variable for API key
-    var APIKey = '&appid=e851c17f69e665b5bbdb5e7f838e3849';
+    // Prompt to API key,  Ln8 is for local testing  
+    // var APIKey = '&appid=e851c17f69e665b5bbdb5e7f838e3849';
+  var APIKey = prompt("Please enter your OpenWeatherMap API key: ");
 
     // Variables to access from HTML
     var form = $("#search-form");
@@ -13,4 +14,12 @@ $(document).ready(function() {
     var today = $("#today");
     var forecast = $("#forecast");
     var history = $("#history");
+
+// Initial form submit function, form sets the event to be triggered when clicked
+// preventDefault prevents the default behaviour such as cauing the page to refresh
+// input gets the value entered and stores it in the var "city"
+    form.on("click", function(event) {
+        event.preventDefault();
+        var city = input.val();
+    })
 })
