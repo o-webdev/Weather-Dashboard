c0a58d0aa184dc40605e56fbcaa4873400632b35
Omar-ahm <irycevier@gmail.com>
2023-01-31 20:14:57 +0000

    debugging to resolve issue with multple buttons being created for history items, changes made to for loop that creates the history buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,6 +30,76 @@ $(document).ready(function () {
     // clear the history section
     history.empty();
 
+    $.ajax({
+        url: todayURL + city + APIKey,
+        method: "GET",
+      }).then(function (data) {
+        // Check to see if today object is empty so it doesnt append and create duplicates
+        today.empty();
+  
+        // variables and dynamic elements to display on page
+        var name = $("<p>");
+        name.text(data.name);
+        name.css({
+          display: "inline-block",
+          "margin-right": "10px",
+          "font-weight": "bold",
+          "font-size": "30px",
+        });
+        var date = $("<p>");
+        date.text(moment().format("DD/MM/YYYY"));
+        date.css({ display: "inline-block", "margin-right": "10px" });
+        var icon = $("<img>");
+        icon.attr(
+          "src",
+          "https://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png"
+        );
+        var hr = $("<hr>");
+        var temperature = $("<p>");
+        temperature.text(
+          "Temperature: " + Math.floor(data.main.temp - 273.15) + " °C"
+        );
+        var humidity = $("<p>");
+        humidity.text("Humidity: " + data.main.humidity);
+        var wind = $("<p>");
+        wind.text("Wind: " + data.wind.speed);
+  
+        $("#today").append(name, date, icon, hr, temperature, humidity, wind);
+        $("#today").css({ "border": "0.5px solid black", "border-radius": "5px", "padding-left": "10px" });
+        });
+        
+        $.ajax ({
+            url:forcastURL + city + APIKey,
+            method: "GET"
+          }).then(function(data) {
+            // Make sure the forecast is empty so it doesnt append
+            forecast.empty();
+    
+            
+            // Loop through the forecast data (every 3 hours)
+            for (var i = 0; i < data.list.length; i--) {
+                // get the forecast for each day
+                if (data.list[i].dt_txt.includes("12:00:00")) {
+    
+                    // Create a forecast card to display forecast information 
+                    var forecastCard = $("<div>").addClass("col-lg-2 forecast-card");
+                    var date = $("<p>");
+                    date.text(moment(data.list[i].dt_txt).format("DD/MM/YYYY"));
+                    var icon = $("<img>");
+                    icon.attr("src", "https://openweathermap.org/img/wn/" + data.list[i].weather[0].icon + "@2x.png");
+                    var temperature = $("<p>");
+                    temperature.text("Temp: " + Math.floor(data.list[i].main.temp -273.15) + " °C");
+                    var humidity = $("<p>");
+                    humidity.text("Humid: " + data.list[i].main.humidity);
+                    var wind = $("<p>");
+                    wind.text("Wind: " + data.list[i].wind.speed);
+    
+                    forecastCard.append(date, icon, temperature, humidity, wind)
+                    forecast.append(forecastCard);
+                    $(".forecast-card").css({"background-color": "#28B8CE", "padding": "5px", "border-radius": "5px" });
+                    $("#forecast").css({"justify-content": "space-between", "text-align": "center", "padding": "20px"})
+                };   
+        
     // Loop through the cityHistory array and creat buttons for each city
     cityHistory.forEach(function (historyCity) {
         var historyButton = $("<button>");
@@ -41,13 +111,16 @@ $(document).ready(function () {
         "border-radius": "5px",
         "margin": "3px",
 });
+history.append(historyButton);
+    });
+};
 
 // add click event to the historyButton
 historyButton.on("click", function() {
     // set the value of the input field to the clicked city
     input.val(historyCity);
 });
-history.append(historyButton);
+
     });
     }
 $("#history").on("click", "button", function() {
@@ -130,4 +203,4 @@ $("#history").on("click", "button", function() {
     });
   });
 });
-})
+});
